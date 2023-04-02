5a266e8659541a94ae6ba3bda756bc7a8d1f9cb5
VINICIUSNUNES137 <90266473+VINICIUSNUNES137@users.noreply.github.com>
2023-04-02 15:27:48 -0300

    fix: api

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 'use strict'
 
 const largura = window.screen.width
-let i = 0
+let guardarClick
 let contUser = 0
 
 const criarCurso = curso => {
 
-  const card = document.createElement('div')
+  const card = document.createElement('a')
+  card.href = "http://127.0.0.1:5500/alunos.html"
+  card.target = '_blank'
   card.classList.add('card')
-  card.id = i
-  i++
+  card.id = curso.sigla
 
   const img = document.createElement('img')
   img.src = curso.icone
-  
+  img.alt = curso.nome
+
   const name = document.createElement('span')
   name.classList.add('card__name')
   name.textContent = curso.sigla
@@ -35,6 +37,36 @@ const carregarCursos = async () => {
   const cards = cursos.map(criarCurso)
 
   container.replaceChildren(...cards)
+
+  cards.forEach(card => {
+    card.onclick = async () => {
+      console.log('cliquei no: ' + card.id);
+      // const url1 = `http://localhost:8080/v1/lion-school/cursos`
+
+      // const response1 = await fetch(url1)
+      // const data1 = await response1.json()
+      // const cursos1 = await data1
+
+      // // location.href = 'http://127.0.0.1:5500/alunos.html'
+
+      // const text = document.createElement('p')
+      // text.textContent = cursos1.nome
+
+      // aaa(text)
+
+
+
+    }
+  })
+
 }
+
+// const img1 = document.createElement('img')
+// img1.src = 'https://raw.githubusercontent.com/VINICIUSNUNES137/lion-school_front/main/img/ds.svg'
+
+// let aaa = document.getElementById('container2')
+
+// aaa.replaceChildren(img1)
+
 
 carregarCursos()
