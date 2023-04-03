01a7400d9f79596bdaba79a734376c805f47b11f
VINICIUSNUNES137 <90266473+VINICIUSNUNES137@users.noreply.github.com>
2023-04-02 23:43:11 -0300

    feat: cards dinamicos

--- js/app.js
+++ js/app.js
@@ -7,8 +7,9 @@ let contUser = 0
 const criarCurso = curso => {
 
   const card = document.createElement('a')
-  card.href = "http://127.0.0.1:5500/alunos.html"
-  card.target = '_blank'
+  // card.href = "../alunos.html"
+  // card.href = "#"
+  // card.target = '_blank'
   card.classList.add('card')
   card.id = curso.sigla
 
@@ -22,51 +23,74 @@ const criarCurso = curso => {
 
   card.append(img, name)
 
+
+  card.addEventListener('click', function () {
+    // window.location.href = '../alunos.html'
+    carregarAluno(card.id)
+  })
+
+
   return card
 }
 
 const carregarCursos = async () => {
+
+  // window.location.href = '../index.html'
 
   const url = 'http://localhost:8080/v1/lion-school/cursos'
 
   const response = await fetch(url)
   const data = await response.json()
   const cursos = await data.cursos
-
+  const containerAluno = document.getElementById('aluno-container')
+  containerAluno.style.width = '0'
   const container = document.getElementById('container')
   const cards = cursos.map(criarCurso)
 
   container.replaceChildren(...cards)
 
-  cards.forEach(card => {
-    card.onclick = async () => {
-      console.log('cliquei no: ' + card.id);
-      // const url1 = `http://localhost:8080/v1/lion-school/cursos`
+}
 
-      // const response1 = await fetch(url1)
-      // const data1 = await response1.json()
-      // const cursos1 = await data1
-
-      // // location.href = 'http://127.0.0.1:5500/alunos.html'
-
-      // const text = document.createElement('p')
-      // text.textContent = cursos1.nome
-
-      // aaa(text)
+carregarCursos()
 
 
+const criarAluno = aluno => {
+  // const curso_name = document.createElement('h2')
+  const cardAluno = document.createElement('div')
+  cardAluno.classList.add('card-aluno')
 
-    }
-  })
+  const imgAluno = document.createElement('img')
+  imgAluno.src = aluno.foto
+  imgAluno.classList.add('aluno-image')
+
+  const nomeAluno = document.createElement('span')
+  nomeAluno.classList.add('aluno-name')
+  nomeAluno.textContent = aluno.nome
+
+  cardAluno.append(imgAluno, nomeAluno)
+
+  return cardAluno
+}
+
+
+const carregarAluno = async (siglaDoCurso) => {
+
+  // const response = await fetch(`http://localhost:8080/v1/lion-school/alunos?curso=${siglaDoCurso}`)
+  const response = await fetch(`http://localhost:8080/v1/lion-school/alunos?curso=${siglaDoCurso}`)
+  const data = await response.json()
+  const alunos = await data.alunos
+
+  const containerAluno = document.getElementById('aluno-container')
+  containerAluno.classList.add('aluno-container')
+  containerAluno.style.width = '100%'
+  const cardAlunos = alunos.map(criarAluno)
+
+
+  const containerCurso = document.getElementById('curso-container')
+  containerCurso.style.display = 'none'
+
+
+  containerAluno.replaceChildren(...cardAlunos)
 
 }
 
-// const img1 = document.createElement('img')
-// img1.src = 'https://raw.githubusercontent.com/VINICIUSNUNES137/lion-school_front/main/img/ds.svg'
-
-// let aaa = document.getElementById('container2')
-
-// aaa.replaceChildren(img1)
-
-
-carregarCursos()
